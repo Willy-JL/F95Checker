84af17e1e7f5131fcc686652554b5644b59db25c
Willy-JL <willy.leslie@icloud.com>
2021-03-09 14:49:28 +0100

    Bump version for release + PACKAGE

--- F95Checker.py
+++ F95Checker.py
@@ -36,7 +36,7 @@ except ImportError as e:
 
 # Setup Globals
 from modules import globals
-globals.version = '7.0dev8'
+globals.version = '7.0'
 globals.login_url = "https://f95zone.to/login/login"
 globals.search_url = "https://f95zone.to/quicksearch"
 globals.notif_url = "https://f95zone.to/conversations/popup"
