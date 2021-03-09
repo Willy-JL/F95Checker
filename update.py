3bf9d8ce4e9ebfe151b871bef6e70c427575cc32
Willy-JL <willy.leslie@icloud.com>
2021-03-09 10:55:53 +0100

    Fix update script

--- update.py
+++ update.py
@@ -1,12 +1,16 @@
 import os
 import sys
+import time
 import zipfile
 import pathlib
 import requests
+from subprocess import Popen
 from clint.textui import progress
 
 
 if __name__ == '__main__':
+    print(f"Pulling update from {sys.argv[1]}...\n")
+    time.sleep(3)
     if len(sys.argv) == 3 and sys.argv[1][:4] == "http" and sys.argv[1][-4:] == ".zip":
         dl = requests.get(sys.argv[1], stream=True)
         with open('update.zip', 'wb') as f:
@@ -17,20 +21,25 @@ if __name__ == '__main__':
                     f.flush()
         for path in list(pathlib.Path('./').rglob('*')):
             full_path = "./" + "/".join(path.parts)
-            if not os.path.isdir(full_path) and not "update.py" and not "update.exe" in path.name and not "update.zip" in path.name:
+            if not os.path.isdir(full_path) and not "update.py" in path.name and not "update.exe" in path.name and not "update.zip" in path.name:
                 try:
                     os.remove(full_path)
                 except (PermissionError, OSError, FileNotFoundError):
                     pass
         for path in list(pathlib.Path('./').rglob('*')):
             full_path = "./" + "/".join(path.parts)
-            if os.path.isdir(full_path) and not "update.py" and not "update.exe" in path.name and not "update.zip" in path.name:
+            if os.path.isdir(full_path) and not "update.py" in path.name and not "update.exe" in path.name and not "update.zip" in path.name:
                 try:
                     os.removedirs(full_path)
                 except (PermissionError, OSError, FileNotFoundError):
                     pass
         with zipfile.ZipFile('update.zip', 'r') as z:
-            z.extractall()
+            for member in z.namelist():
+                try:
+                    z.extract(member)
+                except:
+                    pass
         os.remove('./update.zip')
         del dl
-        os.system(f'"{sys.argv[2]}"')
+        Popen([sys.argv[2]])
+        sys.exit(0)
