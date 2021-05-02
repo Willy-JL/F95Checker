d46cc60b9601d377b0d7d39934f21a3bd3d78dcc
Willy-JL <willy.leslie@icloud.com>
2021-05-02 02:24:36 +0200

    Bump version for release

--- F95Checker.py
+++ F95Checker.py
@@ -36,7 +36,7 @@ except ImportError as e:
 
 # Setup Globals
 from modules import globals
-globals.version = '8.0h1'
+globals.version = '8.1'
 
 globals.domain            = "https://f95zone.to"
 globals.check_login_page  = globals.domain +  "/account/"
