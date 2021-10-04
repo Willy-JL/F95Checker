756a85bbd21f970da0007c9fb83dec5892c245ef
Willy-JL <willy.leslie@icloud.com>
2021-10-04 20:14:17 +0200

    Add globals typehints for useful IDE suggestions

--- modules/globals.py
+++ modules/globals.py
@@ -1,40 +1,50 @@
-version           = None
+from modules.gui import F95CheckerTray, F95CheckerGUI
+from PyQt5.QtWidgets import QApplication
+from modules.singleton import Singleton
+from qasync import QSelectorEventLoop
+from PyQt5.QtCore import QSettings
+from aiohttp import ClientSession
+from PyQt5.QtGui import QFont
+from asyncio import Semaphore
 
-app               = None
-font_awesome      = None
-gui               = None
-http              = None
-image_bg_tasks    = None
-image_semaphore   = None
-loop              = None
-settings          = None
-singleton         = None
-tray              = None
 
-config            = None
-config_path       = None
-exec_type         = None
-mode              = None
-token             = None
-user_browsers     = None
-user_os           = None
+version           : str                = None
 
-bg_paused         = None
-checked_updates   = None
-checking_updates  = None
-logged_in         = None
-logging_in        = None
-refreshing        = None
-updated_games     = None
-warned_connection = None
+app               : QApplication       = None
+font_awesome      : QFont              = None
+gui               : F95CheckerGUI      = None
+http              : ClientSession      = None
+image_bg_tasks    : set                = None
+image_semaphore   : Semaphore          = None
+loop              : QSelectorEventLoop = None
+settings          : QSettings          = None
+singleton         : Singleton          = None
+tray              : F95CheckerTray     = None
 
-alerts_page       = None
-check_login_page  = None
-domain            = None
-inbox_page        = None
-login_endpoint    = None
-login_page        = None
-notif_endpoint    = None
-qsearch_endpoint  = None
-tool_page         = None
-two_step_endpoint = None
+config            : dict               = None
+config_path       : str                = None
+exec_type         : str                = None
+mode              : str                = None
+token             : str                = None
+user_browsers     : dict               = None
+user_os           : str                = None
+
+bg_paused         : bool               = None
+checked_updates   : bool               = None
+checking_updates  : bool               = None
+logged_in         : bool               = None
+logging_in        : bool               = None
+refreshing        : bool               = None
+updated_games     : bool               = None
+warned_connection : bool               = None
+
+alerts_page       : str                = None
+check_login_page  : str                = None
+domain            : str                = None
+inbox_page        : str                = None
+login_endpoint    : str                = None
+login_page        : str                = None
+notif_endpoint    : str                = None
+qsearch_endpoint  : str                = None
+tool_page         : str                = None
+two_step_endpoint : str                = None
