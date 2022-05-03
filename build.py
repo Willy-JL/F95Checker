a1823ed50ec9d53bf8c0a97726f54be328c7c4f8
Willy-JL <willy.leslie@icloud.com>
2022-05-03 18:47:21 +0200

    Try to fix libffi + PACKAGE

--- build.py
+++ build.py
@@ -1,11 +1,29 @@
+from ctypes.util import find_library
 import cx_Freeze
 import sys
+import re
+import os
 
 
 base = None
 if sys.platform.startswith("win"):
     # Hide console on Windows
     base = "Win32GUI"
+
+bin_includes = []
+if sys.platform.startswith("linux"):
+    # Bundle libffi.so
+    library = find_library("ffi")
+    expr = r'%s\s+\([^\)]+\) => ([^\n]+)' % re.escape(library)
+    f = os.popen('/sbin/ldconfig -p 2>/dev/null')
+    data = ""
+    try:
+        data = f.read()
+    finally:
+        f.close()
+    res = re.search(expr, data)
+    if res:
+        bin_includes.append(res.group(1))
 
 cx_Freeze.setup(
     name="F95Checker",
@@ -25,6 +43,7 @@ cx_Freeze.setup(
             "packages": [
                 "OpenGL",
             ],
+            "bin_includes": bin_includes,
             "include_files": [
                 "resources",
                 "LICENSE"
