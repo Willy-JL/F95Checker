25a6faa8f08a630634defc2e1445780026f8aed7
Willy-JL <willy.leslie@icloud.com>
2022-05-03 22:09:17 +0200

    Fix ffi for good + PACKAGE

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
-# from ctypes.util import find_library
+from ctypes.util import find_library
 import cx_Freeze
-# import tempfile
-# import pathlib
-# import shutil
 import sys
-# import re
-# import os
 
 
 base = None
@@ -13,23 +8,10 @@ if sys.platform.startswith("win"):
     # Hide console on Windows
     base = "Win32GUI"
 
-# bin_includes = []
-# if sys.platform.startswith("linux"):
-#     # Bundle libffi.so on Linux
-#     name = find_library("ffi")
-#     expr = r'%s\s+\([^\)]+\) => ([^\n]+)' % re.escape(name)
-#     f = os.popen('/sbin/ldconfig -p 2>/dev/null')
-#     data = ""
-#     try:
-#         data = f.read()
-#     finally:
-#         f.close()
-#     res = re.search(expr, data)
-#     if res:
-#         path = pathlib.Path(res.group(1)).resolve()
-#         temp = pathlib.Path(tempfile.mkdtemp())
-#         library = shutil.copy(path, temp / name)
-#         bin_includes.append(library)
+bin_includes = []
+if sys.platform.startswith("linux"):
+    # Bundle libffi.so on Linux
+    bin_includes.append(find_library("ffi"))
 
 cx_Freeze.setup(
     name="F95Checker",
@@ -48,13 +30,13 @@ cx_Freeze.setup(
             "optimize": 1,
             "packages": [
                 "OpenGL",
-                "cffi"
             ],
-            # "bin_includes": bin_includes,
+            "bin_includes": bin_includes,
             "include_files": [
                 "resources",
                 "LICENSE"
             ],
+            "silent_level": 1,
             "include_msvcr": True
         }
     }
