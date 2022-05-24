577fab7d8981fae7d52d2f3acb420d9b4d4b2fb5
Willy-JL <willy.leslie@icloud.com>
2022-05-24 23:52:07 +0200

    Fix windows index error on missing browser + BUILD

--- modules/globals.py
+++ modules/globals.py
@@ -98,7 +98,7 @@ if sys.platform.startswith("win"):
                     break
                 except Exception:
                     pass
-        if browser.path[0] == '"' and browser.path [-1] == '"':
+        if browser.path and browser.path[0] == '"' and browser.path [-1] == '"':
             browser.path = browser.path[1:-1]
 else:
     for browser in list(Browser):
