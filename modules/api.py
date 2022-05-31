1cbc72dd61d2325954e6f34b9bee137a3159b970
Willy-JL <willy.leslie@icloud.com>
2022-05-31 10:54:20 +0200

    Implement dummy refresh process structure

--- modules/api.py
+++ modules/api.py
@@ -1,9 +1,11 @@
 import subprocess
 import aiohttp
 import asyncio
+import random
 import shlex
 import json
 
+from modules.structs import Game, Status
 from modules import globals, async_thread, db
 
 session: aiohttp.ClientSession = None
@@ -52,12 +54,49 @@ async def login():
     await db.update_cookies(new_cookies)
 
 
+async def check(game: Game):
+    try:
+        await asyncio.sleep(random.random())
+        print(game.id)
+    except asyncio.CancelledError:
+        print(f"cancelled {game.id}")
+        raise
+
+
+async def check_notifs():
+    try:
+        await asyncio.sleep(random.random())
+        print("notifs")
+    except asyncio.CancelledError:
+        print("cancelled notifs")
+        raise
+
+
 async def refresh():
     globals.refresh_progress = 0
     globals.refresh_total = 1
+
     if not await is_logged_in():
         await login()
         if not await is_logged_in():
             return
+
+    refresh_tasks = asyncio.Queue()
+    async def worker():
+        while not refresh_tasks.empty():
+            await refresh_tasks.get_nowait()
+            globals.refresh_progress += 1
+
+    for game in globals.games.values():
+        if game.status is Status.Completed and not globals.settings.refresh_completed_games:
+            continue
+        refresh_tasks.put_nowait(check(game))
+    refresh_tasks.put_nowait(check_notifs())
+
     globals.refresh_progress += 1
-    print("logged in!")
+    globals.refresh_total += refresh_tasks.qsize()
+
+    try:
+        await asyncio.gather(*[worker() for _ in range(globals.settings.refresh_workers)])
+    except asyncio.CancelledError:
+        return
