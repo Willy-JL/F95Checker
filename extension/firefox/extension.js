771871b3c8ab70e8afb13f462471dbdba1166de3
Willy-JL <willy.leslie@icloud.com>
2022-06-17 23:44:38 +0200

    Browser addon

---
+++ extension/firefox/extension.js
@@ -0,0 +1,42 @@
+function objectToXml(object) {
+    if (object instanceof Array || Object.keys(object).length !== 1) {
+        throw 'variable has to be an object with a single property'
+    }
+    return variableToXml(object)
+}
+function variableToXml(variable, arrayItemPropertyName = null) {
+    if (Array.isArray(variable)) {
+        return variable.reduce((xml, propertyValue) => {
+            const value = variableToXml(propertyValue)
+            return `${xml}<${arrayItemPropertyName}>${value}</${arrayItemPropertyName}>`
+        }, '')
+    }
+    if (variable instanceof Object) {
+        return Object.entries(variable).reduce((xml, [propertyName, propertyValue]) => {
+            const value = variableToXml(propertyValue, propertyName )
+            const tag = propertyValue instanceof Array ? value : `<${propertyName}>${value}</${propertyName}>`
+            return `${xml}${tag}`
+        }, '')
+    }
+    return variable
+}
+
+chrome.browserAction.onClicked.addListener(tab => {
+    fetch("http://localhost:57095/", {
+        method: "POST",
+        body: objectToXml({
+            methodCall: {
+                methodName: "add_game",
+                params: {
+                    param: [
+                        {
+                            value: {
+                                string: tab.url
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+    });
+});
--- modules/rpc_thread.py
+++ modules/rpc_thread.py
@@ -1,5 +1,6 @@
 import xmlrpc.server
 import threading
+import asyncio
 
 from modules import globals, async_thread, callbacks, utils
 
@@ -12,8 +13,18 @@ def start():
 
     def run_loop():
         server = xmlrpc.server.SimpleXMLRPCServer(("localhost", globals.rpc_port), logRequests=False, allow_none=True)
+
         server.register_function(globals.gui.show, "show_window")
-        server.register_function(lambda url: [async_thread.run(callbacks.add_games(*utils.extract_thread_matches(url)))], "add_game")
+        def add_game(url):
+            if matches := utils.extract_thread_matches(url):
+                globals.gui.show()
+                async def _add_game():
+                    await asyncio.sleep(0.1)
+                    await callbacks.add_games(*matches)
+                async_thread.run(_add_game())
+            return "success"
+        server.register_function(add_game)
+
         server.serve_forever()
 
     thread = threading.Thread(target=run_loop, daemon=True)
