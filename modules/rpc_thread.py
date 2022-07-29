f95e19b01379f65ccf3b669967a2f75dfe1ad0e7
Willy-JL <willy.leslie@icloud.com>
2022-07-29 22:32:00 +0200

    Show better popup for rpc errors

--- modules/rpc_thread.py
+++ modules/rpc_thread.py
@@ -2,7 +2,8 @@ import xmlrpc.server
 import threading
 import asyncio
 
-from modules import globals, async_thread, callbacks, utils
+from modules import globals, async_thread, callbacks, msgbox, utils
+from modules.structs import MsgBox
 
 server: xmlrpc.server.SimpleXMLRPCServer = None
 thread: threading.Thread = None
@@ -12,7 +13,10 @@ def start():
     global server, thread
 
     def run_loop():
-        server = xmlrpc.server.SimpleXMLRPCServer(("localhost", globals.rpc_port), logRequests=False, allow_none=True)
+        try:
+            server = xmlrpc.server.SimpleXMLRPCServer(("localhost", globals.rpc_port), logRequests=False, allow_none=True)
+        except Exception:
+            raise msgbox.Exc("RPC server error", f"Failed to start RPC server on localhost port {globals.rpc_port}. This means that the web\nbrowser extension will not work, while F95Checker itself should be unaffected.\n\nThis can often be caused by:\n - Hyper-V\n - Docker\n - Antivirus or firewall", MsgBox.warn, more=utils.get_traceback())
 
         server.register_function(globals.gui.show, "show_window")
         def add_game(url):
