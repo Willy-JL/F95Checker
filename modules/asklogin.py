1a6bfb84e84d2cf1786aa0d72a7682fd26a97bd2
Willy-JL <willy.leslie@icloud.com>
2022-08-12 22:58:15 +0200

    Login window progress bar and more improvements

--- modules/asklogin.py
+++ modules/asklogin.py
@@ -55,19 +55,69 @@ def asklogin(url: str):
 
     # Show login window
     app = QtWidgets.QApplication(sys.argv)
-    weblogin = QCookieWebEngineView()
-    weblogin.setWindowTitle("Please login...")
-    weblogin.resize(500, 720)
-    def check_login(*_):
-        if "xf_user" in weblogin.cookies:
-            weblogin.close()
-    weblogin.loadFinished.connect(check_login)
-    weblogin.show()
-    weblogin.setUrl(QtCore.QUrl(url))
+    window = QtWidgets.QWidget()
+    window.setLayout(QtWidgets.QGridLayout())
+    window.layout().setContentsMargins(0, 0, 0, 0)
+    window.layout().setSpacing(0)
+    window.setStyleSheet("""
+        QProgressBar {
+            background: #000000;
+            border-radius: 0px;
+        }
+        QProgressBar::chunk {
+            background: #0066FF;
+            border-radius: 0px;
+        }
+        QLabel {
+            color: #FFFFFF;
+            font-size: 5pt;
+        }
+    """)
+    progress = QtWidgets.QProgressBar(window)
+    progress.setTextVisible(False)
+    progress.setFixedHeight(10)
+    progress.setMaximum(100)
+    window.layout().addWidget(progress, 0, 0)
+    label = QtWidgets.QLabel(text="Click to reload")
+    window.layout().addWidget(label, 0, 0, QtCore.Qt.AlignmentFlag.AlignCenter)
+    webview = QCookieWebEngineView(window)
+    window.setWindowTitle("Login to F95Zone")
+    window.resize(500, 720)
+    loading = [False]
+    def load_started(*_):
+        loading[0] = True
+        progress.setValue(1)
+        progress.repaint()
+        window.setWindowTitle("Login to F95Zone  |  Loading...")
+    def load_progress(value):
+        progress.setValue(max(1, value))
+        progress.repaint()
+    def load_finished(*_):
+        loading[0] = False
+        progress.setValue(0)
+        progress.repaint()
+        window.setWindowTitle("Login to F95Zone")
+        if "xf_user" in webview.cookies:
+            window.close()
+    webview.loadStarted.connect(load_started)
+    webview.loadProgress.connect(load_progress)
+    webview.loadFinished.connect(load_finished)
+    window.layout().addWidget(webview, 1, 0)
+    def reload(*_):
+        if loading[0]:
+            webview.stop()
+            load_finished()
+        else:
+            webview.reload()
+            load_started()
+    progress.mousePressEvent = reload
+    label.mousePressEvent = reload
+    window.show()
+    webview.setUrl(QtCore.QUrl(url))
     app.exec()
 
     # Restore stdout and pass cookies
     sys.stdout.close()
     os.dup2(saved_stdout_fd, original_stdout_fd)
     sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-    sys.stdout.write(json.dumps(weblogin.cookies))
+    sys.stdout.write(json.dumps(webview.cookies))
