8052113eeee8aa69dfe9671b9b3e7a237b377e7c
Willy-JL <willy.leslie@icloud.com>
2022-10-26 19:27:06 +0100

    Better image loader

--- modules/imagehelper.py
+++ modules/imagehelper.py
@@ -7,139 +7,150 @@ import imgui
 from modules import sync_thread
 
 redraw = False
+_dummy_texture_id = None
+
+
+def dummy_texture_id():
+    global _dummy_texture_id
+    if _dummy_texture_id is None:
+        _dummy_texture_id = gl.glGenTextures(1)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, _dummy_texture_id)
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, 0, 0, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, b"\x00\x00\x00\xff")
+    return _dummy_texture_id
+
+
+def get_rgba_pixels(image: Image.Image):
+    if image.mode == "RGB":
+        return image.tobytes("raw", "RGBX")
+    else:
+        if image.mode != "RGBA":
+            image = image.convert("RGBA")
+        return image.tobytes("raw", "RGBA")
 
 
 class ImageHelper:
     def __init__(self, path: str | pathlib.Path, glob=""):
         self.width = 1
         self.height = 1
+        self.frame = -1
         self.glob = glob
+        self.elapsed = 0.0
         self.loaded = False
         self.loading = False
         self.applied = False
         self.missing = False
         self.invalid = False
-        self.frame_count = 1
-        self.animated = False
         self.prev_time = 0.0
-        self.current_frame = -1
-        self.frame_elapsed = 0.0
-        self.data: bytes | list[bytes] = None
-        self._texture_id: int = None
-        self.frame_durations: list[float] = None
-        self.path = pathlib.Path(path)
+        self.animated = False
+        self.frames: list[bytes] = []
+        self.durations: list[float] = []
+        self.texture_ids: list[int] = []
         self.resolved_path: pathlib.Path = None
+        self.path: pathlib.Path = pathlib.Path(path)
         self.resolve()
-
-    def reset(self):
-        gl.glBindTexture(gl.GL_TEXTURE_2D, self._texture_id)
-        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, 0, 0, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, b"\x00\x00\x00\xff")
-        self.applied = False
-
-    @staticmethod
-    def get_rgba_pixels(image: Image.Image):
-        if image.mode == "RGB":
-            return image.tobytes("raw", "RGBX")
-        else:
-            if image.mode != "RGBA":
-                image = image.convert("RGBA")
-            return image.tobytes("raw", "RGBA")
-
-    def set_missing(self):
-        self.missing = True
-        self.loaded = True
-        self.loading = False
 
     def resolve(self):
         self.resolved_path = self.path
         if self.glob:
             paths = list(self.resolved_path.glob(self.glob))
             if not paths:
-                self.set_missing()
+                self.missing = True
                 return
             # If you want you can setup preferred extensions like this:
             paths.sort(key=lambda path: path.suffix != ".gif")
             # This will prefer .gif files!
             self.resolved_path = paths[0]
-        if self.resolved_path.is_file():
-            self.missing = False
-        else:
-            self.set_missing()
-            return
+        self.missing = not self.resolved_path.is_file()
 
     def reload(self):
+        self.loaded = False
+        self.loading = True
+        self.applied = False
         self.resolve()
+
+        self.frame = -1
+        self.elapsed = 0.0
+        self.frames.clear()
+        self.invalid = False
+        self.animated = False
+        self.durations.clear()
+        self.width, self.height = (1, 1)
+
         if self.missing:
+            self.loaded = True
+            self.loading = False
             return
+
         try:
             image = Image.open(self.resolved_path)
-            self.invalid = False
         except UnidentifiedImageError:
             self.invalid = True
             self.loaded = True
             self.loading = False
             return
+
         self.width, self.height = image.size
-        if hasattr(image, "n_frames") and image.n_frames > 1:
-            self.animated = True
-            self.data = []
-            self.frame_durations = []
-            for frame in ImageSequence.Iterator(image):
-                self.data.append(self.get_rgba_pixels(frame))
-                if (duration := image.info["duration"]) < 1:
-                    duration = 100
-                self.frame_durations.append(duration / 1250)
-                # Technically this should be / 1000 (millis to seconds) but I found that 1250 works better...
-            self.frame_count = len(self.data)
-        else:
-            self.data = self.get_rgba_pixels(image)
+        for frame in ImageSequence.Iterator(image):
+            self.frames.append(get_rgba_pixels(frame))
+            if (duration := frame.info.get("duration", 0)) < 1:
+                duration = 100
+            self.durations.append(duration / 1250)
+            # Technically this should be / 1000 (millis to seconds) but I found that 1250 works better...
+        self.animated = len(self.durations) > 1
+
+        image.close()
         self.loaded = True
         self.loading = False
 
-    def apply(self, data: bytes):
-        gl.glBindTexture(gl.GL_TEXTURE_2D, self._texture_id)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_BORDER)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_BORDER)
-        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, self.width, self.height, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, data)
+    def apply(self):
+        if self.texture_ids:
+            gl.glDeleteTextures([self.texture_ids])
+            self.texture_ids.clear()
+        for frame in self.frames:
+            texture_id = gl.glGenTextures(1)  # TODO: generate all together
+            self.texture_ids.append(texture_id)
+            gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_BORDER)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_BORDER)
+            gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, self.width, self.height, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, frame)
+        self.frames.clear()
+        self.applied = True
 
     @property
     def texture_id(self):
-        if self._texture_id is None:
-            self._texture_id = gl.glGenTextures(1)
         if not self.loaded:
             if not self.loading:
                 self.loading = True
-                self.reset()
+                self.applied = False
                 # This next self.reload() actually loads the image and does all the conversion. It takes time and resources!
                 # self.reload()
                 # You can (and maybe should) run this in a thread! threading.Thread(target=self.reload, daemon=True).start()
                 # Or maybe setup an image thread and queue images to load one by one?
                 # You could do this with https://gist.github.com/Willy-JL/bb410bcc761f8bf5649180f22b7f3b44
                 sync_thread.queue(self.reload)
-        elif not self.missing:
-            if self.animated:
-                if self.prev_time != (new_time := imgui.get_time()):
-                    self.prev_time = new_time
-                    self.frame_elapsed += imgui.get_io().delta_time
-                    while (excess := self.frame_elapsed - self.frame_durations[max(self.current_frame, 0)]) > 0:
-                        self.frame_elapsed = excess
-                        self.applied = False
-                        self.current_frame += 1
-                        if self.current_frame == self.frame_count:
-                            self.current_frame = 0
-                if not self.applied:
-                    self.apply(self.data[self.current_frame])
-                    self.applied = True
-            elif not self.applied:
-                self.apply(self.data)
-                self.applied = True
-        return self._texture_id
+            return dummy_texture_id()
+
+        if self.missing or self.invalid:
+            return dummy_texture_id()
+
+        if not self.applied:
+            self.apply()
+
+        if self.animated:
+            if self.prev_time != (new_time := imgui.get_time()):
+                self.prev_time = new_time
+                self.elapsed += imgui.get_io().delta_time
+                while (excess := self.elapsed - self.durations[max(self.frame, 0)]) > 0:
+                    self.elapsed = excess
+                    self.frame += 1
+                    if self.frame == len(self.durations) - 1:
+                        self.frame = 0
+
+        return self.texture_ids[self.frame]
 
     def render(self, width: int, height: int, *args, **kwargs):
-        if self.missing:
-            return False
         if imgui.is_rect_visible(width, height):
             if self.animated or self.loading:
                 global redraw
