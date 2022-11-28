1cb109a827f9ed789667654eda77e17e64f6aa7f
Willy-JL <willy.leslie@icloud.com>
2022-11-28 22:42:36 +0000

    Read icons from file and get icon names

--- modules/icons.py
+++ modules/icons.py
@@ -1,58 +1,55 @@
-account_outline = "󰀓"
-alert_circle = "󰀨"
-alert_octagon = "󰀩"
-alert_rhombus = "󱇎"
-arrow_left_drop_circle = "󰁒"
-arrow_right_drop_circle = "󰁙"
-arrow_up_thick = "󰁞"
-bell_badge_outline = "󰅸"
-book_clock = "󱚈"
-book_information_variant = "󱁯"
-cancel = "󰜺"
-check = "󰄬"
-checkbox_marked_circle = "󰄳"
-clipboard_text_off_outline = "󱘡"
-clipboard_text_outline = "󰨸"
-close_circle = "󰅙"
-cloud_download = "󰅢"
-content_copy = "󰆏"
-content_paste = "󰆒"
-counter = "󰆙"
-cursor_move = "󰆾"
-draw_pen = "󱦹"
-file = "󰈔"
-flag_checkered = "󰈼"
-folder = "󰉋"
-folder_edit_outline = "󰷎"
-folder_open_outline = "󰷏"
-folder_remove_outline = "󰮞"
-github = "󰊤"
-information = "󰋼"
-information_off_outline = "󱞍"
-information_outline = "󰋽"
-information_variant = "󰙎"
-label_multiple_outline = "󱍶"
-label_off_outline = "󰫌"
-label_outline = "󰌖"
-lightning_bolt_circle = "󰠠"
-link_variant = "󰌹"
-message_star = "󰚚"
-motion_play_outline = "󱖑"
-open_in_new = "󰏌"
-pause_circle = "󰏥"
-pen = "󰏪"
-play = "󰐊"
-pound = "󰐣"
-refresh = "󰑐"
-reload_alert = "󱄋"
-star = "󰓎"
-star_circle = "󰓏"
-star_outline = "󰓒"
-star_shooting = "󱝁"
-tag_multiple_outline = "󱋷"
-tag_off_outline = "󱈡"
-tag_outline = "󰓼"
-trash_can_outline = "󰩺"
-update = "󰚰"
-view_agenda_outline = "󱇘"
-view_grid_outline = "󱇙"
+import builtins
+import struct
+
+from modules import globals
+
+names: dict[str, str] = {}
+
+
+def _():
+    font = next(globals.self_path.glob("resources/fonts/materialdesignicons-webfont.*.ttf")).read_bytes()
+    def unpack(fmt: str, size: int, offset: int):
+        return struct.unpack(fmt, font[offset:offset + size])[0]
+    ulong_size  = 4
+    ushort_size = 2
+    ubyte_size  = 1
+    ulong  = lambda offset: unpack(">L", ulong_size,  offset)
+    ushort = lambda offset: unpack(">H", ushort_size, offset)
+    ubyte  = lambda offset: unpack(">B", ubyte_size,  offset)
+
+    cmap_offset = ulong(font.find(b"cmap") + len(b"cmap") + ulong_size)
+    cmap_tables = ushort(cmap_offset + ushort_size)
+    for table_i in range(cmap_tables):
+        cmap_table_offset = cmap_offset + ulong(cmap_offset + 2*ushort_size + table_i*(2*ushort_size + ulong_size) + 2*ushort_size)
+        cmap_table_format = ushort(cmap_table_offset)
+        if cmap_table_format == 12:
+            break
+
+    encr_offset = cmap_table_offset
+    groups = ulong(encr_offset + 2*ushort_size + 2*ulong_size)
+    groups_offset = encr_offset + 2*ushort_size + 3*ulong_size
+    glyphs_chars = {}
+    for group_i in range(groups):
+        group_offset = groups_offset + group_i*(3*ulong_size)
+        char_num = ulong(group_offset)
+        glyph_id = ulong(group_offset + 2*ulong_size)
+        glyphs_chars[glyph_id] = chr(char_num)
+
+    post_offset = ulong(font.find(b"post") + len(b"post") + ulong_size)
+    glyphs = ushort(post_offset + 2*4 + 2*2 + 5*ulong_size)
+    glyph_offset = post_offset + 2*4 + 2*2 + 5*ulong_size + ushort_size + glyphs*ushort_size + ubyte_size
+    module = builtins.globals()
+    names_chars = {}
+    for glyph_i in range(glyphs - 1):
+        glyph_id = glyph_i + 1
+        name_size = ubyte(glyph_offset)
+        glyph_offset += ubyte_size
+        name = str(font[glyph_offset:glyph_offset + name_size], encoding="utf-8")
+        glyph_offset += name_size
+        names_chars[name] = glyphs_chars[glyph_id]
+
+    for name in sorted(names_chars):
+        char = names_chars[name]
+        names[name] = char
+        module[name.replace("-", "_")] = char
+_()
