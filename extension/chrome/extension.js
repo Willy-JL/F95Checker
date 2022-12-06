7f4f0332bc95d9b13947cb710725c7a88bf028d9
Willy-JL <willy.leslie@icloud.com>
2022-12-06 20:31:07 +0000

    Web extension show library icons

--- extension/chrome/extension.js
+++ extension/chrome/extension.js
@@ -3,7 +3,10 @@ const rpcURL = `http://localhost:${rpcPort}`;
 let games = [];
 
 
-async function rpcCall(method, path, body, tab) {
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+const rpcCall = async (method, path, body, tabId) => {
     try {
         const res = await fetch(`${rpcURL}${path}`, {
             method: method,
@@ -14,9 +17,9 @@ async function rpcCall(method, path, body, tab) {
         }
         return res;
     } catch {
-        if (tab) {
+        if (tabId) {
             chrome.scripting.executeScript({
-                target: {tabId: tab.id},
+                target: { tabId: tabId },
                 func: () => { alert("Could not connect to F95Checker!\nIs it open and updated? Is RPC enabled?") }
             });
         }
@@ -24,25 +27,99 @@ async function rpcCall(method, path, body, tab) {
 }
 
 
-async function getGames() {
+const getGames = async () => {
     const res = await rpcCall("GET", "/games");
-    if (res) {
-        games = res.json();
-    } else {
-        games = [];
-    }
+    games = res ? await res.json() : [];
 }
 
 
-async function addGame(url, tab) {
-    await rpcCall("POST", "/games/add", JSON.stringify([url]), tab);
+const addGame = async (url, tabId) => {
+    await rpcCall("POST", "/games/add", JSON.stringify([url]), tabId);
+    await sleep(0.5 * 1000)
+    await updateIcons(tabId);
+}
+
+
+// Add library icons for added games
+const updateIcons = async (tabId) => {
     await getGames();
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: (games) => {
+            const threadId = (url) => {
+                const match = /threads\/(?:(?:[^\.\/]*)\.)?(\d+)/.exec(url);
+                return match ? parseInt(match[1]) : null;
+            }
+            const createIcon = (isImage) => {
+                const icon = document.createElement("i");
+                icon.classList.add("fa", "fa-box-check", "f95checker-library-icon");
+                icon.style.color = "#EC5555";
+                if (isImage) {
+                    icon.style.position = "absolute";
+                    icon.style.zIndex = "999";
+                    icon.style.textShadow = "0px 0px 5px #222";
+                    icon.style.right = "5px";
+                    icon.style.top = "7px";
+                    icon.style.fontSize = "larger";
+                }
+                icon.setAttribute("title", "This game is present in your F95Checker library!");
+                icon.addEventListener("click", () => {
+                    alert("This game is present in your F95Checker library!");
+                });
+                return icon;
+            }
+            const doUpdate = () => {
+                for (elem of document.getElementsByClassName("f95checker-library-icon")) {
+                    elem.remove();
+                }
+                let done = [];
+                for (elem of document.querySelectorAll('a[href*="/threads/"]')) {
+                    const id = threadId(elem.href);
+                    if (!id || !games.includes(id)) {
+                        continue;
+                    }
+                    let isDone = false;
+                    for (doneElem of done) {
+                        if (doneElem.contains(elem)) {
+                            isDone = true;
+                            break;
+                        }
+                    }
+                    if (isDone) {
+                        continue;
+                    }
+                    const isImage = elem.classList.contains("resource-tile_link") || elem.parentNode.parentNode.classList.contains("es-slides");
+                    if (!isImage && !elem.href.endsWith("/unread")) {
+                        continue;
+                    }
+                    done.push(elem.parentNode);
+                    elem.insertAdjacentElement("beforebegin", createIcon(isImage));
+                }
+                const title = document.getElementsByClassName("p-title-value")[0];
+                if (title && games.includes(threadId(document.location))) {
+                    title.insertBefore(createIcon(false), title.childNodes[title.childNodes.length - 1]);
+                }
+            }
+            doUpdate();
+            const latest = document.getElementById("latest-page_items-wrap");
+            if (latest) {
+                const observer = new MutationObserver(() => {
+                    doUpdate();
+                });
+                observer.observe(latest, { attributes: true });
+            }
+        },
+        args: [games]
+    });
 }
+chrome.webNavigation.onCompleted.addListener((details) => {
+    updateIcons(details.tabId);
+}, { url: [{ hostSuffix: "f95zone.to" }] });
 
 
 // Click on extension icon
 chrome.action.onClicked.addListener(tab => {
-    addGame(tab.url, tab);
+    addGame(tab.url, tab.id);
 });
 
 
@@ -62,10 +139,10 @@ chrome.runtime.onInstalled.addListener(async () => {
     });
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    addGame(info.linkUrl || info.pageUrl, tab);
+    addGame(info.linkUrl || info.pageUrl, tab.id);
 });
 
 
 // Get game list every 5 minutes
-setInterval(getGames, 5 * 60 * 1000)
+setInterval(getGames, 5 * 60 * 1000);
 getGames();
