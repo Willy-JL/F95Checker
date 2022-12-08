c2079ee260163d181c28eb0b6d8893b83d6b2f51
Willy-JL <willy.leslie@icloud.com>
2022-12-08 23:08:00 +0000

    Bump 9.6.1 + BUILD

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import contextlib
 import pathlib
 import sys
 
-version = "9.6"
+version = "9.6.1"
 release = False
 build_number = 0
 version_name = f"{version}{'' if release else ' beta'}{'' if release or not build_number else ' ' + str(build_number)}"
