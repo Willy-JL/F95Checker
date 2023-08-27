577d0bd8b15d39cb294bda611299e5f57266135e
littleraisins <littleraisins@proton.me>
2023-08-27 13:00:47 +0300

    Added Prettier config, and applied formatting

--- extension/chrome/extension.js
+++ extension/chrome/extension.js
@@ -3,15 +3,13 @@ const rpcURL = `http://localhost:${rpcPort}`;
 let games = [];
 let reminders = [];
 
-
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const rpcCall = async (method, path, body, tabId) => {
     try {
         const res = await fetch(`${rpcURL}${path}`, {
             method: method,
-            body: body
+            body: body,
         });
         if (!res.ok) {
             throw res.status;
@@ -21,37 +19,37 @@ const rpcCall = async (method, path, body, tabId) => {
         if (tabId) {
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
-                func: () => { alert("Could not connect to F95Checker!\nIs it open and updated? Is RPC enabled?") }
+                func: () => {
+                    alert(
+                        'Could not connect to F95Checker!\nIs it open and updated? Is RPC enabled?'
+                    );
+                },
             });
         }
     }
-}
-
+};
 
 const getData = async () => {
     let res = null;
 
-    res = await rpcCall("GET", "/games");
+    res = await rpcCall('GET', '/games');
     games = res ? await res.json() : [];
 
-    res = await rpcCall("GET", "/reminders");
+    res = await rpcCall('GET', '/reminders');
     reminders = res ? await res.json() : [];
-}
-
+};
 
 const addGame = async (url, tabId) => {
-    await rpcCall("POST", "/games/add", JSON.stringify([url]), tabId);
-    await sleep(0.5 * 1000)
+    await rpcCall('POST', '/games/add', JSON.stringify([url]), tabId);
+    await sleep(0.5 * 1000);
     await updateIcons(tabId);
-}
-
+};
 
 const addReminder = async (url, tabId) => {
-    await rpcCall("POST", "/reminders/add", JSON.stringify([url]), tabId);
-    await sleep(0.5 * 1000)
+    await rpcCall('POST', '/reminders/add', JSON.stringify([url]), tabId);
+    await sleep(0.5 * 1000);
     await updateIcons(tabId);
-}
-
+};
 
 // Add icons for games and reminders
 const updateIcons = async (tabId) => {
@@ -62,152 +60,164 @@ const updateIcons = async (tabId) => {
             const extractThreadId = (url) => {
                 const match = /threads\/(?:(?:[^\.\/]*)\.)?(\d+)/.exec(url);
                 return match ? parseInt(match[1]) : null;
-            }
+            };
             const createContainer = () => {
-                const c = document.createElement("div");
-                c.classList.add("f95checker-library-icons");
-                c.style.display = "inline-flex";
-                c.style.gap = "5px";
-                c.style.padding = "3px 3px";
+                const c = document.createElement('div');
+                c.classList.add('f95checker-library-icons');
+                c.style.display = 'inline-flex';
+                c.style.gap = '5px';
+                c.style.padding = '3px 3px';
                 return c;
-            }
+            };
             const gamesIcon = () => {
-                const icon = document.createElement("i");
+                const icon = document.createElement('i');
                 icon.style.fontFamily = "'Font Awesome 5 Pro'";
-                icon.classList.add("fa", "fa-box-heart");
-                icon.setAttribute("title", "This game is present in your F95Checker library!");
-                icon.addEventListener("click", () => alert("This game is present in your F95Checker library!"));
-                icon.style.color = "#FD5555";
+                icon.classList.add('fa', 'fa-box-heart');
+                icon.setAttribute('title', 'This game is present in your F95Checker library!');
+                icon.addEventListener('click', () =>
+                    alert('This game is present in your F95Checker library!')
+                );
+                icon.style.color = '#FD5555';
                 return icon;
-            }
+            };
             const remindersIcon = (id) => {
-                const icon = document.createElement("i");
-                const text = reminders.find(b => b.id === id).notes || "<Empty note>";
+                const icon = document.createElement('i');
+                const text = reminders.find((b) => b.id === id).notes || '<Empty note>';
                 icon.style.fontFamily = "'Font Awesome 5 Pro'";
-                icon.classList.add("fa", "fa-sticky-note");
-                icon.setAttribute("title", text);
-                icon.addEventListener("click", () => alert(text));
-                icon.style.color = "#55eecc";
+                icon.classList.add('fa', 'fa-sticky-note');
+                icon.setAttribute('title', text);
+                icon.addEventListener('click', () => alert(text));
+                icon.style.color = '#55eecc';
                 return icon;
-            }
+            };
             const removeOldIcons = () => {
-                document.querySelectorAll('.f95checker-library-icons').forEach(e => e.remove());
-            }
+                document.querySelectorAll('.f95checker-library-icons').forEach((e) => e.remove());
+            };
             const addThreadIcons = () => {
                 for (elem of document.querySelectorAll('a[href*="/threads/"]')) {
-
                     const id = extractThreadId(elem.href);
 
-                    if (!id || ![...games, ...reminders.map(r => r.id)].includes(id)) {
+                    if (!id || ![...games, ...reminders.map((r) => r.id)].includes(id)) {
                         continue;
                     }
 
-                    const isImage = elem.classList.contains("resource-tile_link") || elem.parentNode.parentNode.classList.contains("es-slides");
+                    const isImage =
+                        elem.classList.contains('resource-tile_link') ||
+                        elem.parentNode.parentNode.classList.contains('es-slides');
+
                     if (!isImage && /page-\d*$/.test(elem.href)) {
                         continue;
                     }
 
                     const container = createContainer();
                     if (games.includes(id)) container.prepend(gamesIcon());
-                    if (reminders.map(r => r.id).includes(id)) container.prepend(remindersIcon(id));
+                    if (reminders.map((r) => r.id).includes(id))
+                        container.prepend(remindersIcon(id));
 
                     if (isImage) {
-                        container.style.position = "absolute";
-                        container.style.zIndex = "50";
-                        container.style.right = "5px";
-                        container.style.top = "5px";
-                        container.style.background = "#262626";
-                        container.style.border = "solid #262626";
-                        container.style.borderRadius = "4px";
-                        container.style.fontSize = "larger";
+                        container.style.position = 'absolute';
+                        container.style.zIndex = '50';
+                        container.style.right = '5px';
+                        container.style.top = '5px';
+                        container.style.background = '#262626';
+                        container.style.border = 'solid #262626';
+                        container.style.borderRadius = '4px';
+                        container.style.fontSize = 'larger';
                     }
 
                     if (!isImage && elem.children.length > 0) {
                         // Search page
                         try {
-                            whitespaces = elem.querySelectorAll('span.label-append')
-                            whitespaces[whitespaces.length - 1].insertAdjacentElement("afterend", container);
+                            whitespaces = elem.querySelectorAll('span.label-append');
+                            lastWhitespace = whitespaces[whitespaces.length - 1];
+                            lastWhitespace.insertAdjacentElement('afterend', container);
                         } catch (e) {
-                            continue
+                            continue;
                         }
-                    }
-                    else if (elem.classList.contains('resource-tile_link')) {
+                    } else if (elem.classList.contains('resource-tile_link')) {
                         // To accomodate all tile layouts on latest updates page
-                        thumb = elem.querySelector('div.resource-tile_thumb')
-                        thumb.insertAdjacentElement("beforebegin", container);
+                        thumb = elem.querySelector('div.resource-tile_thumb');
+                        thumb.insertAdjacentElement('beforebegin', container);
                     } else {
                         // Everywhere else
-                        elem.insertAdjacentElement("beforebegin", container);
+                        elem.insertAdjacentElement('beforebegin', container);
                     }
                 }
-            }
+            };
             const addPageIcon = () => {
                 const id = extractThreadId(document.location);
                 const container = createContainer();
-                container.style.marginInlineEnd = "6px";
-                const title = document.getElementsByClassName("p-title-value")[0];
+                container.style.marginInlineEnd = '6px';
+                const title = document.getElementsByClassName('p-title-value')[0];
                 if (title) {
                     if (games.includes(id)) container.prepend(gamesIcon());
-                    if (reminders.map(r => r.id).includes(id)) container.prepend(remindersIcon(id));
-                    if (container.firstChild) title.insertBefore(container, title.childNodes[title.childNodes.length - 1]);
+                    if (reminders.map((r) => r.id).includes(id))
+                        container.prepend(remindersIcon(id));
+                    if (container.firstChild)
+                        title.insertBefore(
+                            container,
+                            title.childNodes[title.childNodes.length - 1]
+                        );
                 }
-            }
+            };
             const doUpdate = () => {
                 removeOldIcons();
                 addThreadIcons();
                 addPageIcon();
-            }
+            };
             const installMutationObservers = () => {
-                const latest = document.getElementById("latest-page_items-wrap");
+                const latest = document.getElementById('latest-page_items-wrap');
                 if (latest) {
                     const observer = new MutationObserver(doUpdate);
                     observer.observe(latest, { attributes: true });
                 }
-            }
+            };
             installMutationObservers();
             doUpdate();
         },
-        args: [games, reminders]
+        args: [games, reminders],
     });
-}
-chrome.webNavigation.onCompleted.addListener((details) => {
-    updateIcons(details.tabId);
-}, { url: [{ hostSuffix: "f95zone.to" }] });
-
+};
+chrome.webNavigation.onCompleted.addListener(
+    (details) => {
+        updateIcons(details.tabId);
+    },
+    { url: [{ hostSuffix: 'f95zone.to' }] }
+);
 
 // Click on extension icon
-chrome.action.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener((tab) => {
     addGame(tab.url, tab.id);
 });
-
 
 // Context menus
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.contextMenus.create({
         id: `add-page-to-f95checker`,
         title: `Add this page to F95Checker`,
-        contexts: ["page"],
-        documentUrlPatterns: ["*://*.f95zone.to/threads/*"]
+        contexts: ['page'],
+        documentUrlPatterns: ['*://*.f95zone.to/threads/*'],
     });
     chrome.contextMenus.create({
         id: `add-link-to-f95checker`,
         title: `Add this link to F95Checker`,
-        contexts: ["link"],
-        targetUrlPatterns: ["*://*.f95zone.to/threads/*"]
+        contexts: ['link'],
+        targetUrlPatterns: ['*://*.f95zone.to/threads/*'],
     });
     chrome.contextMenus.create({
         id: `add-page-to-reminders`,
         title: `Add this page to reminders`,
-        contexts: ["page"],
-        documentUrlPatterns: ["*://*.f95zone.to/threads/*"]
+        contexts: ['page'],
+        documentUrlPatterns: ['*://*.f95zone.to/threads/*'],
     });
     chrome.contextMenus.create({
         id: `add-link-to-reminders`,
         title: `Add this link to reminders`,
-        contexts: ["link"],
-        targetUrlPatterns: ["*://*.f95zone.to/threads/*"]
+        contexts: ['link'],
+        targetUrlPatterns: ['*://*.f95zone.to/threads/*'],
     });
 });
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
         case 'add-link-to-f95checker':
@@ -221,7 +231,5 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
 });
 
-
-// Get data every 5 minutes
-setInterval(getData, 5 * 60 * 1000);
+setInterval(getData, 5 * 60 * 1000); // 5 minutes
 getData();
--- extension/integrated.js
+++ extension/integrated.js
@@ -11,47 +11,42 @@ rpcURL = `http://localhost:${rpcPort}`;
 games = [];
 reminders = [];
 
-sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
+sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 rpcCall = async (method, path, body) => {
     try {
         const res = await fetch(`${rpcURL}${path}`, {
             method: method,
-            body: body
+            body: body,
         });
         if (!res.ok) {
             throw res.status;
         }
         return res;
     } catch {}
-}
-
+};
 
 getData = async () => {
     let res = null;
 
-    res = await rpcCall("GET", "/games");
+    res = await rpcCall('GET', '/games');
     games = res ? await res.json() : [];
 
-    res = await rpcCall("GET", "/reminders");
+    res = await rpcCall('GET', '/reminders');
     reminders = res ? await res.json() : [];
-}
-
+};
 
 addGame = async (url) => {
-    await rpcCall("POST", "/games/add", JSON.stringify([url]));
-    await sleep(0.5 * 1000)
+    await rpcCall('POST', '/games/add', JSON.stringify([url]));
+    await sleep(0.5 * 1000);
     await updateIcons();
-}
-
+};
 
 addReminder = async (url, tabId) => {
-    await rpcCall("POST", "/reminders/add", JSON.stringify([url]), tabId);
-    await sleep(0.5 * 1000)
+    await rpcCall('POST', '/reminders/add', JSON.stringify([url]), tabId);
+    await sleep(0.5 * 1000);
     await updateIcons(tabId);
-}
-
+};
 
 // Add icons for games and reminders
 updateIcons = async () => {
@@ -59,108 +54,113 @@ updateIcons = async () => {
     const extractThreadId = (url) => {
         const match = /threads\/(?:(?:[^\.\/]*)\.)?(\d+)/.exec(url);
         return match ? parseInt(match[1]) : null;
-    }
+    };
     const createContainer = () => {
-        const c = document.createElement("div");
-        c.classList.add("f95checker-library-icons");
-        c.style.display = "inline-flex";
-        c.style.gap = "5px";
-        c.style.padding = "3px 3px";
+        const c = document.createElement('div');
+        c.classList.add('f95checker-library-icons');
+        c.style.display = 'inline-flex';
+        c.style.gap = '5px';
+        c.style.padding = '3px 3px';
         return c;
-    }
+    };
     const gamesIcon = () => {
-        const icon = document.createElement("i");
+        const icon = document.createElement('i');
         icon.style.fontFamily = "'Font Awesome 5 Pro'";
-        icon.classList.add("fa", "fa-box-heart");
-        icon.setAttribute("title", "This game is present in your F95Checker library!");
-        icon.addEventListener("click", () => alert("This game is present in your F95Checker library!"));
-        icon.style.color = "#FD5555";
+        icon.classList.add('fa', 'fa-box-heart');
+        icon.setAttribute('title', 'This game is present in your F95Checker library!');
+        icon.addEventListener('click', () =>
+            alert('This game is present in your F95Checker library!')
+        );
+        icon.style.color = '#FD5555';
         return icon;
-    }
+    };
     const remindersIcon = (id) => {
-        const icon = document.createElement("i");
-        const text = reminders.find(b => b.id === id).notes || "<Empty note>";
+        const icon = document.createElement('i');
+        const text = reminders.find((b) => b.id === id).notes || '<Empty note>';
         icon.style.fontFamily = "'Font Awesome 5 Pro'";
-        icon.classList.add("fa", "fa-sticky-note");
-        icon.setAttribute("title", text);
-        icon.addEventListener("click", () => alert(text));
-        icon.style.color = "#55eecc";
+        icon.classList.add('fa', 'fa-sticky-note');
+        icon.setAttribute('title', text);
+        icon.addEventListener('click', () => alert(text));
+        icon.style.color = '#55eecc';
         return icon;
-    }
+    };
     const removeOldIcons = () => {
-        document.querySelectorAll('.f95checker-library-icons').forEach(e => e.remove());
-    }
+        document.querySelectorAll('.f95checker-library-icons').forEach((e) => e.remove());
+    };
     const addThreadIcons = () => {
         for (elem of document.querySelectorAll('a[href*="/threads/"]')) {
-
             const id = extractThreadId(elem.href);
 
-            if (!id || ![...games, ...reminders.map(r => r.id)].includes(id)) {
+            if (!id || ![...games, ...reminders.map((r) => r.id)].includes(id)) {
                 continue;
             }
 
-            const isImage = elem.classList.contains("resource-tile_link") || elem.parentNode.parentNode.classList.contains("es-slides");
+            const isImage =
+                elem.classList.contains('resource-tile_link') ||
+                elem.parentNode.parentNode.classList.contains('es-slides');
+
             if (!isImage && /page-\d*$/.test(elem.href)) {
                 continue;
             }
 
             const container = createContainer();
             if (games.includes(id)) container.prepend(gamesIcon());
-            if (reminders.map(r => r.id).includes(id)) container.prepend(remindersIcon(id));
+            if (reminders.map((r) => r.id).includes(id)) container.prepend(remindersIcon(id));
 
             if (isImage) {
-                container.style.position = "absolute";
-                container.style.zIndex = "50";
-                container.style.right = "5px";
-                container.style.top = "5px";
-                container.style.background = "#262626";
-                container.style.border = "solid #262626";
-                container.style.borderRadius = "4px";
-                container.style.fontSize = "larger";
+                container.style.position = 'absolute';
+                container.style.zIndex = '50';
+                container.style.right = '5px';
+                container.style.top = '5px';
+                container.style.background = '#262626';
+                container.style.border = 'solid #262626';
+                container.style.borderRadius = '4px';
+                container.style.fontSize = 'larger';
             }
 
             if (!isImage && elem.children.length > 0) {
                 // Search page
                 try {
-                    whitespaces = elem.querySelectorAll('span.label-append')
-                    whitespaces[whitespaces.length - 1].insertAdjacentElement("afterend", container);
+                    whitespaces = elem.querySelectorAll('span.label-append');
+                    lastWhitespace = whitespaces[whitespaces.length - 1];
+                    lastWhitespace.insertAdjacentElement('afterend', container);
                 } catch (e) {
-                    continue
+                    continue;
                 }
-            }
-            else if (elem.classList.contains('resource-tile_link')) {
+            } else if (elem.classList.contains('resource-tile_link')) {
                 // To accomodate all tile layouts on latest updates page
-                thumb = elem.querySelector('div.resource-tile_thumb')
-                thumb.insertAdjacentElement("beforebegin", container);
+                thumb = elem.querySelector('div.resource-tile_thumb');
+                thumb.insertAdjacentElement('beforebegin', container);
             } else {
                 // Everywhere else
-                elem.insertAdjacentElement("beforebegin", container);
+                elem.insertAdjacentElement('beforebegin', container);
             }
         }
-    }
+    };
     const addPageIcon = () => {
         const id = extractThreadId(document.location);
         const container = createContainer();
-        container.style.marginInlineEnd = "6px";
-        const title = document.getElementsByClassName("p-title-value")[0];
+        container.style.marginInlineEnd = '6px';
+        const title = document.getElementsByClassName('p-title-value')[0];
         if (title) {
             if (games.includes(id)) container.prepend(gamesIcon());
-            if (reminders.map(r => r.id).includes(id)) container.prepend(remindersIcon(id));
-            if (container.firstChild) title.insertBefore(container, title.childNodes[title.childNodes.length - 1]);
+            if (reminders.map((r) => r.id).includes(id)) container.prepend(remindersIcon(id));
+            if (container.firstChild)
+                title.insertBefore(container, title.childNodes[title.childNodes.length - 1]);
         }
-    }
+    };
     const doUpdate = () => {
         removeOldIcons();
         addThreadIcons();
         addPageIcon();
-    }
+    };
     const installMutationObservers = () => {
-        const latest = document.getElementById("latest-page_items-wrap");
+        const latest = document.getElementById('latest-page_items-wrap');
         if (latest) {
             const observer = new MutationObserver(doUpdate);
             observer.observe(latest, { attributes: true });
         }
-    }
+    };
     installMutationObservers();
     doUpdate();
 };
