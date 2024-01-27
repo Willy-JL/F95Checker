941a5de6a18f4c9a11ec9be6f8f77f08d8ea4301
r37r05p3C7 <153987701+r37r05p3C7@users.noreply.github.com>
2024-01-28 00:28:33 +0200

    Injecting custom MDI webfont

--- pack-extension.sh
+++ pack-extension.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+fontname=materialdesignicons-webfont.7.4.47.ttf
+
+cp ./resources/fonts/$fontname ./extension/chrome/icons
 cd ./extension/
 rm ./chrome.zip || true
 rm ./firefox.zip || true
@@ -7,3 +10,5 @@ cd ./chrome/
 zip -r ../chrome.zip *
 cd ../firefox/
 zip -r ../firefox.zip *
+cd ..
+rm ./chrome/icons/$fontname
