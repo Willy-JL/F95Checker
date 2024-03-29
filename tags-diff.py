419d3ce81d727bfaa45ea68e380d7ef8e116532f
r37r05p3C7 <153987701+r37r05p3C7@users.noreply.github.com>
2024-03-29 08:00:23 +0200

    Modified to work in CI/CD pipeline

--- tags-diff.py
+++ tags-diff.py
@@ -1,6 +1,8 @@
+import os
+import sys
 import json
 
-from modules import async_thread, db
+from modules import async_thread, db, globals
 from modules.structs import Tag
 from modules.parser import html
 from modules import api
@@ -10,8 +12,16 @@ def main():
     async_thread.setup()
 
     with db.setup(), api.setup():
+        if xf_user := os.environ.get("XF_USER", None):
+            globals.cookies["xf_user"] = xf_user
+        if xf_tfa_trust := os.environ.get("XF_TFA_TRUST", None):
+            globals.cookies["xf_tfa_trust"] = xf_tfa_trust
         res = async_thread.wait(api.fetch("GET", f"{api.host}/sam/latest_alpha"))
-        txt = html(res).find("body").find("script").text  # type: ignore
+        try:
+            txt = html(res).find("body").find("script").text  # type: ignore
+        except Exception:
+            print("Failed to authenticate! Your auth cookies are stale or missing.")
+            sys.exit(1)
         txt = txt.replace("var latestUpdates =", "").replace(";", "").strip()
         obj = json.loads(txt)
         tags: dict[str, str] = obj["tags"]
@@ -25,8 +35,10 @@ def main():
         print("Some tags are missing:")
         for tag in missing:
             print(f"  - {tag}")
+        sys.exit(1)
     else:
         print("All tags are up-to-date!")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
