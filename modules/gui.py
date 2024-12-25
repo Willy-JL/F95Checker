3e87615bd9f345b28437c14a0960a7d7d2a16692
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-12-25 19:11:46 +0100

    Improve some tooltips, less begin_tooltip()

--- modules/gui.py
+++ modules/gui.py
@@ -1246,15 +1246,20 @@ class MainGUI():
         if imgui.is_item_hovered():
             imgui.begin_tooltip()
             imgui.push_text_wrap_pos(min(imgui.get_font_size() * 35, imgui.io.display_size.x))
-            imgui.text("This game has new tags that F95Checker failed to recognize:")
-            for tag in game.unknown_tags:
-                imgui.text(f" - {tag}")
-            imgui.text("To copy them:")
-            imgui.text(f"{icons.menu_right} Shift + Left click")
-            imgui.text(f"{icons.menu_right} Use Copy button in Tags section")
-            imgui.text("To remove this marker:")
-            imgui.text(f"{icons.menu_right} Middle click")
-            imgui.text(f"{icons.menu_right} Alt + Left click")
+            imgui.text(
+                "This game has new tags that F95Checker failed to recognize:\n"
+                + "\n".join(f" - {tag}" for tag in game.unknown_tags)
+            )
+            imgui.text(
+                "To copy them:\n"
+                f"{icons.menu_right} Shift + Left click\n"
+                f"{icons.menu_right} Use Copy button in Tags section\n"
+            )
+            imgui.text(
+                "To remove this marker:\n"
+                f"{icons.menu_right} Middle click\n"
+                f"{icons.menu_right} Alt + Left click\n"
+            )
             imgui.pop_text_wrap_pos()
             imgui.end_tooltip()
         if imgui.is_item_clicked(imgui.MOUSE_BUTTON_MIDDLE):
@@ -1281,17 +1286,14 @@ class MainGUI():
                 self.filters.append(flt)
             imgui.end_group()
         if imgui.is_item_hovered():
-            imgui.begin_tooltip()
-            imgui.push_text_wrap_pos(min(imgui.get_font_size() * 35, imgui.io.display_size.x))
-            imgui.text(
+            self.draw_hover_text(
                 "This game is archived!\n"
                 "In this state you won't receive update notifications for\n"
                 "this game and it will stay at the bottom of the list.\n"
                 "Middle click to remove it from the archive, alternatively\n"
-                "use the right click menu to do the same."
+                "use the right click menu to do the same.",
+                text=None,
             )
-            imgui.pop_text_wrap_pos()
-            imgui.end_tooltip()
         if imgui.is_item_clicked(imgui.MOUSE_BUTTON_MIDDLE):
             game.archived = False
 
@@ -1598,12 +1600,11 @@ class MainGUI():
         if clicked:
             callbacks.clipboard_copy(str(game.id))
         if imgui.is_item_hovered():
-            imgui.begin_tooltip()
-            imgui.text(
+            self.draw_hover_text(
                 f"Thread ID: {game.id}\n"
-                "Click to copy!"
+                "Click to copy!",
+                text=None,
             )
-            imgui.end_tooltip()
         if game.custom:
             imgui.pop_disabled()
 
@@ -1863,8 +1864,7 @@ class MainGUI():
             imgui.pop_style_color()
             timestamp_size = imgui.get_item_rect_size()
             if imgui.is_item_hovered():
-                with imgui.begin_tooltip():
-                    imgui.text(date.strftime(globals.settings.timestamp_format))
+                self.draw_hover_text(date.strftime(globals.settings.timestamp_format), text=None)
             # Draw message
             message_pos = (timestamp_pos.x, timestamp_pos.y + timestamp_size.y - self.scaled(2))
             imgui.set_cursor_screen_pos(message_pos)
@@ -1964,8 +1964,8 @@ class MainGUI():
                     self.draw_game_tab_widget(game)
 
                 for attr in ("name", "version"):
-                    old_val =  getattr(old_game, attr) or "Unknown"
-                    new_val =  getattr(game, attr) or "Unknown"
+                    old_val = getattr(old_game, attr) or "Unknown"
+                    new_val = getattr(game, attr) or "Unknown"
                     if new_val != old_val:
                         imgui.spacing()
                         imgui.text_disabled(f"{attr.title()}: ")
@@ -2219,9 +2219,7 @@ class MainGUI():
                     game.last_launched = time.time()
                     game.add_timeline_event(TimelineEventType.GameLaunched, "date set manually")
                 if imgui.is_item_hovered():
-                    imgui.begin_tooltip()
-                    imgui.text("Click to set as launched right now!")
-                    imgui.end_tooltip()
+                    self.draw_hover_text("Click to set as launched right now!", text=None)
 
                 imgui.table_next_row()
 
@@ -3263,8 +3261,7 @@ class MainGUI():
                                 imgui.same_line()
                                 imgui.text_disabled(f"({game.votes})")
                             if imgui.is_item_hovered():
-                                with imgui.begin_tooltip():
-                                    imgui.text(f"{utils.bayesian_average(game.score, game.votes):.2f}")
+                                self.draw_hover_text(f"Weighted: {utils.bayesian_average(game.score, game.votes):.2f}", text=None)
                 # Row hitbox
                 imgui.same_line()
                 imgui.set_cursor_pos_y(imgui.get_cursor_pos_y() - imgui.style.frame_padding.y)
@@ -3510,6 +3507,8 @@ class MainGUI():
             cluster = True
         if cols.score.enabled:
             _cluster_text(cols.score.name, f"{game.score:.1f} ({game.votes})")
+            if imgui.is_item_hovered():
+                self.draw_hover_text(f"Weighted: {utils.bayesian_average(game.score, game.votes):.2f}", text=None)
         if cols.last_updated.enabled:
             _cluster_text(cols.last_updated.name, game.last_updated.display or "Unknown")
         if cols.last_launched.enabled:
@@ -4181,14 +4180,14 @@ class MainGUI():
             cant_install_extension = set.browser.integrated or not set.rpc_enabled
             def cant_install_extension_tooltip():
                 if imgui.is_item_hovered():
-                    imgui.begin_tooltip()
-                    imgui.push_text_wrap_pos(min(imgui.get_font_size() * 35, imgui.io.display_size.x))
-                    if set.browser.integrated:
-                        imgui.text("You have selected the Integrated browser, this already includes the extension!")
-                    elif not set.rpc_enabled:
-                        imgui.text("RPC must be enabled for the browser extension to work!")
-                    imgui.pop_text_wrap_pos()
-                    imgui.end_tooltip()
+                    self.draw_hover_text(
+                        "You have selected the Integrated browser, this already includes the extension!"
+                        if set.browser.integrated else
+                        "RPC must be enabled for the browser extension to work!"
+                        if not set.rpc_enabled else
+                        "Huh?",
+                        text=None,
+                    )
             if cant_install_extension:
                 imgui.push_disabled()
             if imgui.button(icons.google_chrome, width=(right_width - imgui.style.item_spacing.x) / 2):
