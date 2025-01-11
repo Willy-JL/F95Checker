d16fb082219369189a9c789286c30335146a5f0d
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2025-01-11 22:14:59 +0000

    Fix Move to New Tab and crash on Close Tab

--- modules/gui.py
+++ modules/gui.py
@@ -2800,14 +2800,15 @@ class MainGUI():
 
     def draw_tabbar(self):
         display_tab = globals.settings.display_tab
+        select_tab = self.current_tab is not display_tab
         if not self.sorts:
             # Fixes incorrect tab for first frame, causing un-shown images to load
             # Sorting would happen later otherwise and tabs would be hidden due to that
             self.current_tab = display_tab
             self.tick_list_columns()
-            select_tab = True
+            save_new_tab = False
         else:
-            select_tab = self.current_tab is not display_tab
+            save_new_tab = True
         new_tab = None
         if Tab.instances and not (globals.settings.filter_all_tabs and self.filtering):
             if imgui.begin_tab_bar("###tabbar", flags=self.tabbar_flags):
@@ -2897,7 +2898,7 @@ class MainGUI():
                                 close_callback()
                         imgui.end_popup()
                 imgui.end_tab_bar()
-        if new_tab is not self.current_tab and not select_tab:
+        if new_tab is not self.current_tab and save_new_tab:
             for game in globals.games.values():
                 game.selected = False
             self.current_tab = new_tab
