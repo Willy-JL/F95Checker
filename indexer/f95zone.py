4bf9d7d32eff2b21baa0fa3daed58e34cd5af72b
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2025-03-25 01:47:00 +0000

    Detect maintenance and error screens in the future

--- indexer/f95zone.py
+++ indexer/f95zone.py
@@ -34,6 +34,8 @@ TEMP_ERROR_MESSAGES = (
     b"<!-- Connection refused -->",
     b"<!-- Too many connections -->",
     b"<p>Automated backups are currently executing. During this time, the site will be unavailable</p>",
+    b"<title>F95Zone :: Scheduled Maintenance</title>",
+    b'<script src="https://static.f95zone.to/assets/SamF95/ErrorPage',
 )
 
 logger = logging.getLogger(__name__)
